9e675413b1264355a1dbea5d7a5e68fb666557c9
Nataniel Latorre <latorre.nataniel@hotmail.com>
2021-11-23 15:09:56 +0100

    sorted answers, minimal css added

--- server.py
+++ server.py
@@ -18,9 +18,13 @@ def list_index():
 def display_question(question_id):
     all_questions = data_handler.data_import(data_handler.DATA_FILE_PATH_QUESTION)
     question = next((q for q in all_questions if q["id"] == question_id), None)
+    question_index = all_questions.index(question)
     all_answers = data_handler.data_import(data_handler.DATA_FILE_PATH_ANSWER)
-    relevant_answers = [a for a in all_answers if a["question_id"] == question_id]
+    relevant_answers = [a for a in all_answers if a['question_id'] == str(question_id)]
+    relevant_answers = sorted(relevant_answers, key=lambda x: x["submission_time"])
     question["view_number"] = int(question["view_number"]) + 1
+    all_questions[question_index] = question
+    data_handler.data_export(data_handler.DATA_FILE_PATH_QUESTION, all_questions, data_handler.DATA_HEADER_QUESTION)
     return render_template("question.html", question=question, answers=relevant_answers)
 
 
