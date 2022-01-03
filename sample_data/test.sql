2f7c4d0a311a49fe47873b3d15afe587435075ac
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-03 13:24:28 +0100

    added emails for users

--- sample_data/test.sql
+++ sample_data/test.sql
@@ -116,8 +116,8 @@ ALTER TABLE ONLY answer
 ALTER TABLE ONLY question
     ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id);
     
-INSERT INTO users VALUES (0, 'test', 'password', '2016-01-01 07:00:00', 0);
-INSERT INTO users VALUES (1, 'first', 'password', '2016-01-02 08:00:00', 0);
+INSERT INTO users VALUES (0, 'test', 'password', '2016-01-01 07:00:00', 'no@none.no', 0);
+INSERT INTO users VALUES (1, 'first', 'password', '2016-01-02 08:00:00', 'email@address.com', 0);
 INSERT INTO question VALUES (0, 0, '2017-04-28 08:29:00', 29, 7, 'How to make lists in Python?', 'I am totally new to this, any hints?', NULL);
 INSERT INTO question VALUES (1, 1, '2017-04-29 09:19:00', 15, 9, 'Wordpress loading multiple jQuery Versions', 'I developed a plugin that uses the jquery booklet plugin (http://builtbywill.com/booklet/#/) this plugin binds a function to $ so I cann call $(".myBook").booklet();
 
