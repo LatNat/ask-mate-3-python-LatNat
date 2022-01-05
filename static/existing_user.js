b2affc60a2e1892cb36d2d7c73e15e0430050d82
Csipke Krisztián <csipkekrisztian@gmail.com>
2022-01-05 13:03:38 +0100

    Modify: Login and register check fix

--- static/existing_user.js
+++ static/existing_user.js
@@ -1,5 +1,8 @@
-window.onload = function() {
-    setTimeout(function(){
+function popUp(){
+setTimeout(function(){
        window.alert("Username or Email already in use, please try another!");
    }, 100);
 }
+window.addEventListener("load", (event) => {
+    popUp();
+});
