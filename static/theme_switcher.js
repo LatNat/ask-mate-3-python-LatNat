f2946595931e3fad907c0258f6351a2b03371256
Nataniel Latorre <latorre.nataniel@hotmail.com>
2022-01-06 15:19:18 +0100

    added: can switch between light/dark themes

--- static/theme_switcher.js
+++ static/theme_switcher.js
@@ -1,3 +1,23 @@
 function switchTheme() {
-    document.getElementsByTagName()
+    let linkedFiles = document.getElementsByTagName("link");
+    let head = document.querySelector("head");
+    let button = document.querySelector("div.theme-button a span");
+    let oldScheme = button.textContent;
+    console.log(oldScheme);
+    let newScheme;
+    if (oldScheme.includes('light')) { newScheme = 'light'} else { newScheme = 'dark'}
+    let newTheme = '<link rel="stylesheet" type="text/css" href="/static/' + newScheme + '_theme_colors.css">'
+    console.log(head);
+    for (let link of linkedFiles) {
+        console.log(link);
+        if (link.href.includes("light")) {
+            link.remove();
+            head.insertAdjacentHTML("afterbegin", newTheme);
+            button.textContent = "light theme";
+        } else if (link.href.includes("dark")) {
+            link.remove();
+            head.insertAdjacentHTML("afterbegin", newTheme);
+            button.textContent = "dark theme";
+        }
+    }
 }
